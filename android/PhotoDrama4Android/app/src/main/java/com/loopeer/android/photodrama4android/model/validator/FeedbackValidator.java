d245f8dc712ae5be532ebc6752047a3cbfa09237
lzz <liuzizhang1994@gmail.com>
2017-04-05 11:45:51 +0800

    add feedback activity

---
+++ android/PhotoDrama4Android/app/src/main/java/com/loopeer/android/photodrama4android/model/validator/FeedbackValidator.java
@@ -0,0 +1,30 @@
+package com.loopeer.android.photodrama4android.model.validator;
+
+import android.databinding.Bindable;
+import android.text.TextUtils;
+import android.util.Log;
+import com.loopeer.databindpack.validator.ObservableValidator;
+
+public class FeedbackValidator extends ObservableValidator {
+
+    public String feedback;
+
+    @Bindable
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+        notifyEnable();
+    }
+
+    @Override public boolean checkEnable() {
+        return !TextUtils.isEmpty(feedback);
+    }
+
+    @Override public boolean isEdited() {
+        return !TextUtils.isEmpty(feedback);
+    }
+
+}
--- android/PhotoDrama4Android/app/src/main/java/com/loopeer/android/photodrama4android/ui/activity/FeedbackActivity.java
+++ android/PhotoDrama4Android/app/src/main/java/com/loopeer/android/photodrama4android/ui/activity/FeedbackActivity.java
@@ -1,4 +1,24 @@
 package com.loopeer.android.photodrama4android.ui.activity;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import com.loopeer.android.photodrama4android.R;
+import com.loopeer.android.photodrama4android.databinding.ActivityFeedBackBinding;
+import com.loopeer.android.photodrama4android.model.validator.FeedbackValidator;
+
 public class FeedbackActivity extends MovieMakerBaseActivity {
+
+    private FeedbackValidator mValidator;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityFeedBackBinding binding = DataBindingUtil.setContentView(this,
+            R.layout.activity_feed_back);
+        binding.setValidator(mValidator = new FeedbackValidator());
+    }
+
+    @Override protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        setCenterTitle(R.string.label_feed_back);
+    }
 }
