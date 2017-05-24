b3a01b55998826fe5ae51d69153ea5d934b7f133
ToDou <yytodou123@gmail.com>
2017-05-24 19:01:45 +0800

    add status test

--- android/PhotoDrama4Android/app/src/main/java/com/loopeer/android/photodrama4android/ui/activity/MainActivity.java
+++ android/PhotoDrama4Android/app/src/main/java/com/loopeer/android/photodrama4android/ui/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.loopeer.android.photodrama4android.ui.activity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.view.View;
 import com.loopeer.android.photodrama4android.Navigator;
@@ -20,6 +22,14 @@ public class MainActivity extends PhotoDramaBaseActivity {
         setContentView(R.layout.activity_main);
         PermissionUtils.checkStoragePermission(this);
 
+      /*  new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    findViewById(R.id.btn_act).setSystemUiVisibility(*//*findViewById(R.id.btn_act).getSystemUiVisibility() | *//*View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+                }
+            }
+        }, 100);*/
     }
 
     @Override
