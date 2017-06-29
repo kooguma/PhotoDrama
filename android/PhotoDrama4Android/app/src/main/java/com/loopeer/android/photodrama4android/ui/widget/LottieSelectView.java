0b3b1b2895b2ecd2d0bb1fc07da6eaa8ed28e7ef
lzz <liuzizhang1994@gmail.com>
2017-06-29 18:57:59 +0800

    fix play-pause animation

--- android/PhotoDrama4Android/app/src/main/java/com/loopeer/android/photodrama4android/ui/widget/LottieSelectView.java
+++ android/PhotoDrama4Android/app/src/main/java/com/loopeer/android/photodrama4android/ui/widget/LottieSelectView.java
@@ -1,33 +1,50 @@
 package com.loopeer.android.photodrama4android.ui.widget;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import com.airbnb.lottie.LottieAnimationView;
 
 public class LottieSelectView extends LottieAnimationView {
 
+    private float speed;
+
     public LottieSelectView(Context context) {
         super(context);
+        init();
     }
 
     public LottieSelectView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public LottieSelectView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        addAnimatorUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override public void onAnimationUpdate(ValueAnimator animation) {
+                float fraction = animation.getAnimatedFraction();
+                if (speed == 1 && fraction >= 0.5f) {
+                    pauseAnimation();
+                }
+            }
+        });
     }
 
     @Override public void setSelected(boolean selected) {
         super.setSelected(selected);
-        if (selected) {
-            setProgress(0.2f);
-            setSpeed(2f);
+        if (!selected) { //0.5 暂停
+            setProgress(0f);
+            setSpeed(speed = 1f);
             playAnimation();
-
         } else {
-            setProgress(0.7f);
-            setSpeed(-2f);
+            setProgress(0.5f);
+            setSpeed(speed = -2.5f);
             playAnimation();
         }
     }
